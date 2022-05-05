641913d0e6b45c876bbf94d622be62efb31eb4c9
oNdsen <a.reinhard@me.com>
2022-05-05 11:49:40 +0200

    File Manager Fix

--- themes/AdminLTE/modules/litefm/litefm.js
+++ themes/AdminLTE/modules/litefm/litefm.js
@@ -1,17 +1,17 @@
 $(document).ready(function()
 {
-	$('.operations-button').addClass('btn btn-xs addClass('btn-primary');
+	$('.operations-button').addClass('btn btn-xs btn-primary');
 	$('.main [href^="?m=gamemanager&p=game_monitor&home_id="]').addClass('btn btn-primary btn-sm');
 
-	$('img[src="images/folder.png"]').replaceWith('<i class="fa fa-folder-open" aria-hidden="true"></i>');
-	$('img[src="images/txt.png"]').replaceWith('<i class="fa fa-pencil-square" aria-hidden="true"></i>');
-        $('img[src="images/exec.png"]').replaceWith('<i class="fa fa-cog" aria-hidden="true"></i>');
+	$('img[src="images/folder.png"]').replaceWith('<i class="fas fa-folder-open"></i>');
+	$('img[src="images/txt.png"]').replaceWith('<i class="fas fa-edit"></i>');
+        $('img[src="images/exec.png"]').replaceWith('<i class="fas fa-cogs"></i>');
 
 	$('.unlocked').children('span').addClass('btn btn-xs btn-danger').css('min-width','80px');
 	$('.locked').children('span').addClass('btn btn-xs btn-success').css('min-width','80px');
 
-	$('[href*="&item="], [href^="javascript:downloadFile"]').addClass('label').addClass('label-default');
-	$('[href$="&p=read_file"]').removeClass('label').removeClass('label-default').addClass('btn').addClass('btn-primary').addClass('btn-xs');
+	$('[href*="&item="], [href^="javascript:downloadFile"]').addClass('badge badge-default');
+	$('[href$="&p=read_file"]').removeClass('badge').removeClass('badge-default').addClass('btn btn-primary btn-xs');
 
 	/* *** Long Name Fix *** */
 	$('td > h3').each(function()
