8b79a04cb5e3d988c19124568e2a101578b1d22f
oNdsen <a.reinhard@me.com>
2022-05-08 14:41:10 +0200

    Modified Dark Variables and some Module Changes

--- themes/AdminLTE/modules/gamemanager/log.js
+++ themes/AdminLTE/modules/gamemanager/log.js
@@ -1,4 +1,10 @@
 $(document).ready(function()
 {
 	$('.main [href^="?m=gamemanager&p=game_monitor&home_id-mod_id-ip-port="]').addClass('btn btn-sm btn-primary');
+	$('.main [name="size"]').addClass('float-right');
+});
+
+$(window).load(function()
+{
+	$('.main [name="remote_send_rcon_command"]').removeClass('btn-sm');
 });
