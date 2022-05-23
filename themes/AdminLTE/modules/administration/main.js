70861f0ce8332e462439142236fa6e8bcc1fdb8b
oNdsen <a.reinhard@me.com>
2022-05-23 13:37:31 +0200

    Administration custom Links

--- themes/AdminLTE/modules/administration/main.js
+++ themes/AdminLTE/modules/administration/main.js
@@ -6,16 +6,45 @@ $(document).ready(function()
 	
 	$(adminButtonTable).find('a.administration-buttons').each(function()
 	{
-		console.log($(this).attr('href'));
 		$('div.administration-table').append('<a class="admin-buttons btn btn-primary d-flex flex-column justify-content-center align-items-center" href="' + $(this).attr('href') + '"><span>' + $(this).find('div').text() + '</span></a>');
 	});
 	
 	$(adminButtonTable).remove();
+	
+	// better remove link buttons
+	$('.remove-button').each(function()
+	{
+		$(this).replaceWith('<button class="btn btn-sm btn-danger"><i class="fas fa-trash mr-1"></i>'+langConsts['OGP_LANG_delete']+'</button>')
+	});
 });
 
 $(window).load(function()
 {
-	$('[name="download"]').prepend('<i class="fas fa-download mr-1"></i>').addClass('btn-secondary');
-	$('[name="restore"]').prepend('<i class="fas fa-upload mr-1"></i>').addClass('btn-warning');
+	$('.main [name="download"]').prepend('<i class="fas fa-download mr-1"></i>').addClass('btn-secondary');
+	$('.main [name="restore"]').prepend('<i class="fas fa-upload mr-1"></i>').addClass('btn-warning');
 	$('.main .custom-file').removeAttr('style');
+	
+	// better links
+	if($('img[src$="link.png"]').length>0)
+	{
+		var parentTable = $('img[src$="link.png"]').closest('.table-responsive');
+		$(parentTable).after('<div class="administration-table externalLinks d-flex flex-wrap justify-content-start"></div>');
+		$(parentTable).find('td').each(function()
+		{
+			$('.externalLinks').append('<div class="elink">'+$(this).html()+'</div>');
+		});
+		$(parentTable).remove();
+	}
+	$('.elink > a').addClass('admin-buttons btn btn-primary d-flex flex-column justify-content-center align-items-center');
+	
+	
+	// remove empty tables
+	$('.main table').each(function()
+	{
+		if($(this).text().trim().length==0)
+		{
+			$(this).parent('.table-responsive').remove();
+		}
+	});
 });
+
