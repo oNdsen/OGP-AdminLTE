9f79b9936213d3083858165a6aeb969355bd3ce7
oNdsen <a.reinhard@me.com>
2022-07-12 13:54:50 +0200

    added icons on buttons

--- themes/AdminLTE/modules/modulemanager/modulemanager.js
+++ themes/AdminLTE/modules/modulemanager/modulemanager.js
@@ -1,5 +1,5 @@
 $(document).ready(function()
 {
-	$('[href^="?m=modulemanager&p=del"]').addClass('btn btn-danger btn-xs');
-	$('[href^="?m=modulemanager&p=add"]').addClass('btn btn-success btn-xs');
+	$('[href^="?m=modulemanager&p=del"]').addClass('btn btn-danger btn-xs').prepend('<i class="fas fa-trash-alt mr-1"></i>');
+	$('[href^="?m=modulemanager&p=add"]').addClass('btn btn-success btn-xs').prepend('<i class="fas fa-plus mr-1"></i>');
 });
