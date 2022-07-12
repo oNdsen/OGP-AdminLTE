b0303af6125173388d529dd532d706b2f8aeab71
oNdsen <a.reinhard@me.com>
2022-07-12 13:54:24 +0200

    hide plaintext passwords on mysql admin

--- themes/AdminLTE/modules/mysql/edit.js
+++ themes/AdminLTE/modules/mysql/edit.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-        $('.card-body [href="?m=mysql&p=mysql_admin"]').addClass('btn btn-sm btn-primary');
+$(document).ready(function()
+{
+	$('.main [href="?m=mysql&p=mysql_admin"]').addClass('btn btn-sm btn-primary');
+	$('.main [id="mysql_root_passwd"], .main [id="db_passwd"]').attr('type', 'password');
+	
+	// reveal pass on focus, hide on leave
+	$('.main [id="mysql_root_passwd"], .main [id="db_passwd"]').on("focus", function()
+	{
+		$(this).attr('type', 'text');
+	}).on("focusout", function()
+	{
+		$(this).attr('type', 'password');
+	});
 });
--- themes/AdminLTE/modules/mysql/mysql_admin.js
+++ themes/AdminLTE/modules/mysql/mysql_admin.js
@@ -1,9 +1,40 @@
-$(document).ready(function() {
-        $('#servermonitor [href$="&delete"], #servermonitor [href$="&remove_db').addClass('btn btn-xs btn-danger');
-        $('#servermonitor [href$="&edit"], #servermonitor [href$="&assign"], #servermonitor [href$="&edit_db_settings"]').addClass('btn btn-xs btn-primary');
+$(document).ready(function()
+{
+	$('#servermonitor [href$="&delete"], #servermonitor [href$="&remove_db').addClass('btn btn-xs btn-danger').prepend('<i class="fas fa-trash-alt mr-1"></i>');;
+	$('#servermonitor [href$="&edit"], #servermonitor [href$="&edit_db_settings"]').addClass('btn btn-xs btn-primary');
 
-        $('#servermonitor .success').addClass('badge').addClass('badge-success').removeClass('success');
-        $('#servermonitor .failure').addClass('badge').addClass('badge-danger').removeClass('failure');
+	$('#servermonitor [href$="&edit"], #servermonitor [href$="&edit_db_settings"]').prepend('<i class="fas fa-cogs mr-1"></i>');
+	$('#servermonitor [href$="&assign"]').addClass('btn btn-xs btn-success').prepend('<i class="fas fa-user-check mr-1"></i>');
 
+	$('#servermonitor .success').addClass('badge badge-success').removeClass('success');
+	$('#servermonitor .failure').addClass('badge badge-danger').removeClass('failure');
+
+	$('.main [id="mysql_root_passwd"]').attr('type', 'password');
+	
+	// reveal pass on focus, hide on leave
+	$('.main [id="mysql_root_passwd"]').on("focus", function()
+	{
+		$(this).attr('type', 'text');
+	}).on("focusout", function()
+	{
+		$(this).attr('type', 'password');
+	});
+	
+	// hide plaintext passwords
+	$('#servermonitor .expand-child').each(function()
+	{
+		var regex = new RegExp(`(\s*)<b>${langConsts['OGP_LANG_mysql_root_passwd']}:<\/b> (.*?)<br>`, "m");
+		var dbInfos = $(this).find('td:first-of-type');
+		
+		var pass = dbInfos.html().match(regex)[2];
+		
+		if(pass)
+		{
+			$(dbInfos).html(function(index,html)
+			{
+				return html.replace(regex, '<b>' + langConsts['OGP_LANG_mysql_root_passwd'] + '</b>: <span class="pass">'+pass+'</span><br>');
+			});
+		}
+	});
 });
 
